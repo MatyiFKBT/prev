e6d899fee0828d6674c5416fb31cf8e6a3e05349
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-06-28 13:58:38 +0000

    chore: bump

--- dist/index.js
+++ dist/index.js
@@ -42,27 +42,18 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(6953));
 const auth_action_1 = __nccwpck_require__(4655);
 const rest_1 = __nccwpck_require__(9349);
-const wait_1 = __nccwpck_require__(7930);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const ms = core.getInput('milliseconds');
-            core.info(JSON.stringify(process.env, null, 2));
-            // auth
-            const auth = (0, auth_action_1.createActionAuth)();
-            const authentication = yield auth();
-            core.debug(authentication.token);
             const octokit = new rest_1.Octokit({ authStrategy: auth_action_1.createActionAuth });
             core.debug('octokit initiated');
             const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-            const { data: { total_count } } = yield octokit.actions.listWorkflowRunsForRepo({ owner, repo });
+            const { data: { total_count, workflow_runs } } = yield octokit.actions.listWorkflowRunsForRepo({ owner, repo, status: 'completed' });
             core.debug('octokit auth response');
             core.info(`total ${total_count}`);
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            const runPromises = workflow_runs.map(run => octokit.actions.deleteWorkflowRun({ run_id: run.id, owner, repo }));
+            yield Promise.all(runPromises);
         }
         catch (error) {
             if (error instanceof Error)
@@ -71,37 +62,6 @@ function run() {
     });
 }
 run();
-
-
-/***/ }),
-
-/***/ 7930:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,19 @@
 import * as core from '@actions/core'
 import { createActionAuth } from '@octokit/auth-action';
 import { Octokit } from '@octokit/rest';
-import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
 
-    core.info(JSON.stringify(process.env,null,2))
-    // auth
-    const auth = createActionAuth();
-    const authentication = await auth();
-    core.debug(authentication.token)
     const octokit = new Octokit({ authStrategy: createActionAuth })
     core.debug('octokit initiated')
-    const [owner,repo] = process.env.GITHUB_REPOSITORY!.split('/');
-    const { data: { total_count } } = await octokit.actions.listWorkflowRunsForRepo({ owner,repo })
+    const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/');
+    const { data: { total_count, workflow_runs } } = await octokit.actions.listWorkflowRunsForRepo({ owner, repo, status: 'completed' })
     core.debug('octokit auth response')
     core.info(`total ${total_count}`)
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const runPromises = workflow_runs.map(run => octokit.actions.deleteWorkflowRun({ run_id: run.id, owner, repo }))
+    await Promise.all(runPromises)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
